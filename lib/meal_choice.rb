68764a891df567231cf7681ddef7f894c6063fde
persuasive-vector-4859 <robdefendini@gmail.com>
2020-03-01 18:13:43 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-def meal_choice(veg1, veg2, protein = 'tofu')
+def meal_choice(veg1, veg2, protein = "tofu")
 
 puts "What a nutritious meal!"
 dinner="A plate of #{protein} with #{veg1} and #{veg2}."
@@ -7,7 +7,7 @@ puts dinner
 end
 
 
- # puts "A plate of #{protein} with #{veg1} and #{veg2}."
+ 
   
 
 # Your code here
